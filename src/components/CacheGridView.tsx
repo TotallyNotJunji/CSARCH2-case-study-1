2b8bc025b97bb4bbe9824a9cabbe6245fdc0bdc7
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2026-08-03 16:39:48 +0800

    feat: implement getTargetIdx logic for LRU/MRU policies

--- src/components/CacheGridView.tsx
+++ src/components/CacheGridView.tsx
@@ -13,11 +13,39 @@ interface CacheGridViewProps {
 
 export default function CacheGridView({
   sets,
-  ways = 8,
+  ways,
   policy,
   highlightSetIndex,
 }: CacheGridViewProps) {
+  
+  // find the target index
+  // each s has 8 cache lines
+  const getTargetIdx = (set: Set) => {
+    const validCacheLines = set.lines
+      .map((line, idx) => ({line, idx}))
+      .filter((item) => item.line.valid);
 
+    if(validCacheLines.length === 0) return -1;
+
+    if(policy === "LRU") {
+      // goes thru each valid cache line to find best minimum value then 
+      // get index
+      let index = validCacheLines.reduce((min, curr) =>
+        // if lastUsed is less than (less recent), keep
+        curr.line.lastUsed < min.line.lastUsed ? curr : min
+      ).idx
+      return index
+    }
+    else if(policy === "MRU") {
+      // goes thru each valid cache line to find best minimum value then 
+      // get index
+      let index = validCacheLines.reduce((min, curr) =>
+        // if lastUsed is more than (mor recent), keep
+        curr.line.lastUsed > min.line.lastUsed ? curr : min
+      ).idx
+      return index
+    }
+  }
   return (
     <div>
 
